318d2a16a014dc55f3b55c2da558e6ad9215d108
jt ciou <jtciou26@gmail.com>
2023-12-29 21:07:57 +0800

    single note

--- src/pages/note.js
+++ src/pages/note.js
@@ -1,22 +1,30 @@
-import React, { useEffect } from "react";
-import { useQuery, gql } from "@apollo/client";
+import React, { useEffect, useState } from "react";
+import { useQuery } from "@apollo/client";
 
 import Note from "../components/Note";
 import { GET_NOTE } from '../gql/query';
 
 
 //將屬性props物件傳遞至元件 動態查詢
-const NotePage = props => {
+const NotePage = (props) => {
     // 將在 url 中找到的id儲存為變數
         const id = props.match.params.id;
     //查詢hook 以變數形式傳遞id值
         const { loading, error, data, refetch } = useQuery(GET_NOTE, { variables: {id} });
+        const [note, setNote] = useState(null);
 
         useEffect(() => {
-            fetchPolicy: 'network-only',
-            console.log('Component mounted, refetching data');
-            refetch();
-          }, [id, refetch]);
+            const fetchData = async () => { 
+                try {
+                    const result = await refretch();
+                        setNote(result.data.note);
+                } catch (error) {
+                    console.error('Error fetching note:', error);
+                }
+             };
+                
+                fetchData();
+            }, [id, refetch]);
 
         if (loading) return <p> Loading... </p>
         if (error) return <p> ERROR! note not found... </p>

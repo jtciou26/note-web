d8efaab4aaa7eac48211cd61b543d4a5edb29180
jt ciou <jtciou26@gmail.com>
2023-12-29 21:24:22 +0800

    single note w useState

--- src/pages/note.js
+++ src/pages/note.js
@@ -4,27 +4,26 @@ import { useQuery } from "@apollo/client";
 import Note from "../components/Note";
 import { GET_NOTE } from '../gql/query';
 
-
 //將屬性props物件傳遞至元件 動態查詢
 const NotePage = (props) => {
     // 將在 url 中找到的id儲存為變數
         const id = props.match.params.id;
     //查詢hook 以變數形式傳遞id值
-        const { loading, error, data, refetch } = useQuery(GET_NOTE, { variables: {id} });
+        const { loading, error, data, refetch } = useQuery(GET_NOTE, { variables: { id } });
         const [note, setNote] = useState(null);
-
+    //use the useState hook to manage the note state. 
+    //The useEffect hook is then used to trigger the data fetch when the component mounts or when the id changes.
         useEffect(() => {
             const fetchData = async () => { 
                 try {
-                    const result = await refretch();
+                    const result = await refetch();
                         setNote(result.data.note);
                 } catch (error) {
                     console.error('Error fetching note:', error);
                 }
              };
-                
-                fetchData();
-            }, [id, refetch]);
+            fetchData();
+        }, [id, refetch]);
 
         if (loading) return <p> Loading... </p>
         if (error) return <p> ERROR! note not found... </p>

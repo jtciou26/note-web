e0b3650207cf2f2d37657d5c59e3e789417fc362
jt ciou <jtciou26@gmail.com>
2024-02-16 17:13:58 +0800

    update react-markdown to 8"

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import ReactMarkdown from 'react-markdown';
+import Markdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 import styled from 'styled-components';
 import NoteUser from './NoteUser';
 import { format } from 'date-fns';
@@ -9,61 +10,61 @@ import { Avatar } from 'antd';
 
 //防止筆記寬度超過800
 const StyledNote = styled.article`
-    max-width: 800px;
-    margin: 0 auto;
-    overflow: hidden;
+  max-width: 800px;
+  margin: 0 auto;
+  overflow: hidden;
 `;
 
 //將筆記中繼資料樣式化
 const MetaData = styled.div`
-    @media (min-width: 500px) {
-        display: flex;
-        align-items: top;
-    }
+  @media (min-width: 500px) {
+    display: flex;
+    align-items: top;
+  }
 `;
 
 //在頭像與中繼資料之間增加一些空間
 const MetaInfo = styled.div`
-    padding-right: 1em;
+  padding-right: 1em;
 `;
 
 //在大型螢幕將UserAction向右對齊
 const UserActions = styled.div`
-    margin-left: auto;
+  margin-left: auto;
 `;
 
 const Note = ({ note }) => {
-    const { loading, error, data } = useQuery(IS_LOGGED_IN);
-    if (loading) return 'Loading...';
-    if (error) return <p>Error! </p>;
-    return ( 
-        <StyledNote>
-            <MetaData>
-                <MetaInfo>
-                    <Avatar
-                        src={note.author.avatar}
-                        alt={note.author.username}
-                        shape="square"
-                        size="large"
-                    />    
-                </MetaInfo>
-                <MetaInfo>
-                    <em>by</em> {note.author.username} <br />
-                    {format(note.createdAt, 'MMM Do YYYY')}
-                </MetaInfo> 
-                {data.isLoggedIn ? (
-                    <UserActions>
-                        <NoteUser note={note} />
-                    </UserActions>
-                ) : (
-                    <UserActions>
-                        <em>Favorites:</em> {note.favoriteCount}
-                    </UserActions>
-                )}
-            </MetaData>
-            <ReactMarkdown source={note.content} />  
-        </StyledNote>
-    );
+  const { loading, error, data } = useQuery(IS_LOGGED_IN);
+  if (loading) return 'Loading...';
+  if (error) return <p>Error! </p>;
+  return (
+    <StyledNote>
+      <MetaData>
+        <MetaInfo>
+          <Avatar
+            src={note.author.avatar}
+            alt={note.author.username}
+            shape="square"
+            size="large"
+          />
+        </MetaInfo>
+        <MetaInfo>
+          <em>by</em> {note.author.username} <br />
+          {format(note.createdAt, 'MMM Do YYYY')}
+        </MetaInfo>
+        {data.isLoggedIn ? (
+          <UserActions>
+            <NoteUser note={note} />
+          </UserActions>
+        ) : (
+          <UserActions>
+            <em>Favorites:</em> {note.favoriteCount}
+          </UserActions>
+        )}
+      </MetaData>
+      <Markdown remarkPlugins={[remarkGfm]} children={note.content} />
+    </StyledNote>
+  );
 };
 
 export default Note;
